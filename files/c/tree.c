58a12a5e3bf453a49fbb65062c8e98a56f6a751f
ShrimpyJ <alejbrock@gmail.com>
2021-10-14 00:00:01 -0400

    Thu Oct 14 12:00:01 AM EDT 2021

--- files/c/tree.c
+++ files/c/tree.c
@@ -4,15 +4,19 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include <dirent.h>
 
 void print_depth(int depth)
 {
 	int i;
 
-	for (i = 0; i < depth; i++){
-		printf("_");
+
+	for (i = 0; i < depth*4; i++){
+    if (i % 4 == 0) printf("│");
+		else printf(" ");
 	}
+  printf("├── ");
 }
 
 void explore_dir(const char *d_name, int depth)
@@ -20,24 +24,42 @@ void explore_dir(const char *d_name, int depth)
 	DIR *dir;
 	struct dirent *entry;
 	struct stat st;
+  char *path;
+  char *name;
+  int len;
 
-	if ((dir = opendir(d_name)) == NULL){
+  /* Construct relative path name */
+  path = calloc(strlen(d_name)+80, sizeof(char));
+  strcat(path, d_name);
+  len = strlen(path);
+
+	if ((dir = opendir(path)) == NULL){
 		fprintf(stderr, "error: could not open directory %s\n", d_name);
 		exit(1);
 	}
 
 	while(entry = readdir(dir)){
-		if (strcmp(entry->d_name, "." ) == 0) continue;
-		if (strcmp(entry->d_name, "..") == 0) continue;
-		stat(entry->d_name, &st);
+    if (entry->d_name[0] == '.') continue;
+    name = calloc(len+strlen(entry->d_name)+3, sizeof(char));
+    //strcat(name, path);
+    strncpy(name, path, len);
+    //strcat(name, "/");
+    strncpy(name+len, "/", 1);
+    //strcat(name, entry->d_name);
+    strncpy(name+len+1, entry->d_name, strlen(entry->d_name));
+		if (lstat(name, &st) != 0){
+      perror(name);
+      exit(1);
+    }
 		print_depth(depth);
 		printf("%s\n", entry->d_name);
 		if (S_ISDIR(st.st_mode)){
-			printf("DIRECTORY '%s' FOUND\n", entry->d_name);
-			explore_dir(entry->d_name, depth+1);
+			explore_dir(name, depth+1);
 		}
+    free(name);
 	}
 
+  free(path);
 	closedir(dir);
 }
 
@@ -45,6 +67,7 @@ int main(int argc, char **argv)
 {
 	int depth;
 
+  printf(".\n");
 	explore_dir(".", 0);
 
 	return 0;
