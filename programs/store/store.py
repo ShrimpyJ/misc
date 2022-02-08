f0188e49577f08fbcc94022a59ce369bbb5bed91
ShrimpyJ <alejbrock@gmail.com>
2022-02-08 00:00:01 -0500

    Tue Feb  8 12:00:01 AM EST 2022

--- programs/store/store.py
+++ programs/store/store.py
@@ -36,20 +36,25 @@ cursor.close()
 db.close()
 
 # Set globals
-TICKRATE = 1    # In seconds
+TICKRATE = 1# / 100000 # In seconds
+SECONDS = int(time.time())
 TIME = datetime.datetime.now()
-DAY = datetime.datetime.today().weekday()
+DAY = datetime.datetime.fromtimestamp(SECONDS).weekday()
 HOUR = ''
 DATE = ''
+REAL_TIME = 0
 
 TRAFFIC = 0
 TRAFFIC_OFFSET = 16   # % for random spread
 TRAFFIC_MULT = 1      # Increase/decrease total traffic
+EMPLOYEES = 0
 
 MIN_ITEMS = 1
 MAX_ITEMS = 250
+MIN_FIND_TIME = 15 # In seconds
+MAX_FIND_TIME = 30 # In seconds
 MIN_SECS_IN_STORE = 5 * 60
-MAX_SECS_IN_STORE = 30 * 60
+MAX_SECS_IN_STORE = 60 * 60
 
 customers = []
 threads = []
@@ -73,7 +78,7 @@ class Employee():
      for x in self.cur:
        s = str(x)
        self.name = s[3:len(s)-4]
-     hour = int(TIME.strftime("%H"))
+       hour = int(TIME.strftime("%H"))
      print(f"{self.name} clocking in at {self.start_time} for {self.hours} hours.", end=' ')
      print(f"Time is {hour}, leaves at {self.end_time}")
 
@@ -91,6 +96,8 @@ class Employee():
     return False
 
   def leave(self):
+    global EMPLOYEES
+    EMPLOYEES -= 1
     lock.acquire()
     hour = int(TIME.strftime("%H"))
     self.cur.execute("SELECT hourly_rate, hours_worked FROM payroll WHERE employee_id=%d" % (self.id))
@@ -102,10 +109,7 @@ class Employee():
     pay = wage * self.hours
     print(f"{self.name} leaving at {hour}. Paid ${wage}/hr for {self.hours} hours (${pay})")
     self.cur.execute("UPDATE payroll SET hours_worked = hours_worked + %d WHERE employee_id = %d" % (hours_worked, self.id))
-    self.cur.execute("UPDATE finance SET wages = wages + %lf WHERE date = '%s'" % (pay, DATE))
-    print(DATE)
-    if self.cur.rowcount == 0:
-      self.cur.execute("INSERT INTO finance (date, sales, wages) VALUES('%s', 0.00, '%lf')" % (DATE, pay))
+    self.cur.execute("INSERT INTO finance (date, sales, wages) VALUES('%s', 0.00, '%lf') ON DUPLICATE KEY UPDATE wages=wages+'%lf'" % (DATE, pay, pay))
     self.conn.commit()
     self.cur.close()
     self.conn.close()
@@ -119,10 +123,14 @@ class Customer():
     self.num_items = randint(MIN_ITEMS, MAX_ITEMS)
     self.list = []
     self.cart = []
+    self.time = 0
     self.spent = Decimal(0.00)
     self.ticks = int((self.num_items / MAX_ITEMS) * MAX_SECS_IN_STORE)
     self.ticks += randint(0, 100)
     self.rate = int(self.ticks / self.num_items)
+    self.start_time = TIME
+    self.end_time =  datetime.datetime.fromtimestamp(SECONDS + self.ticks)
+    self.n_items = self.num_items
     for i in range(self.num_items):
       self.list.append(ITEMS[randint(0, len(ITEMS))-1])
 
@@ -146,38 +154,35 @@ class Customer():
     lock.release()
 
   def shop(self):
-    while self.ticks > 0 and self.num_items > 0:
-      if self.ticks % self.rate == 0:
+    while True:
+      if TIME >= self.end_time:
+        break
+      if self.num_items >= 0:
         self.num_items -= 1
-        if self.num_items < 0:
-          break
         item = self.list[self.num_items]
-        if self.item_available(item) == True:
+        if self.item_available(item):
           self.add_item(item)
-      self.ticks -= 1
+      self.time += 1
       sleep(TICKRATE)
     self.checkout()
 
   def checkout(self):
     lock.acquire()
-    print(f"    {self.name} checking out")
     for item in self.cart:
       self.cur.execute("SELECT price FROM items WHERE id=%d" % (item))
       for x in self.cur:
         price = convert_to_decimal(x)
-      print(f"      {self.name} buying item for {price}")
+      #print(f"      {self.name} buying item for {price}")
       self.spent += price
-    print(f"      {self.name} total: {self.spent}")
-    self.cur.execute("UPDATE finance SET sales = sales + %lf WHERE date = '%s'" % (self.spent, DATE))
-    if self.cur.rowcount == 0:
-      self.cur.execute("INSERT INTO finance (date, sales, wages) VALUES('%s', '%lf', 0.00)" % (DATE, self.spent))
-    print(self.cur.rowcount, "record(s) affected")
+    #print(f"      {self.name} total: {self.spent}")
+
+    self.cur.execute("INSERT INTO finance (date, sales, wages) VALUES('%s', '%lf', 0.00) ON DUPLICATE KEY UPDATE sales=sales+'%lf'" % (DATE, self.spent, self.spent))
     self.conn.commit()
     self.leave()
 
   def leave(self):
     global TRAFFIC
-    print(f"    {self.name} leaving store with {self.num_items} items left to buy and {self.ticks} seconds")
+    print(f"    {self.name} leaving store")
     TRAFFIC -= 1
     self.cur.close()
     self.conn.close()
@@ -190,7 +195,7 @@ def get_db():
     password="storekey",
     database="store"
   )
-  cur = conn.cursor(buffered=True)
+  cur = conn.cursor()
   return conn, cur
     
 def convert_to_decimal(decObj):
@@ -232,11 +237,12 @@ def now():
 
 def print_now():
   print(WEEK[DAY], end=' ')
-  print(TIME.strftime("%d/%m/%Y %H:%M:%S"), end = ' ')
-  print(f"Traffic: {TRAFFIC}")
+  print(TIME.strftime("%d/%m/%Y %H:%M:%S"), end =' ')
+  print(f"Traffic: {TRAFFIC}", end=' ')
+  print(f"Employees: {EMPLOYEES}")
 
-def get_timestamp(seconds):
-  return str(datetime.timedelta(seconds = seconds))
+def get_time():
+  return datetime.datetime.fromtimestamp(SECONDS)
 
 def get_hour():
   hour = int(TIME.strftime("%H"))
@@ -258,12 +264,18 @@ def get_hour_traffic(days):
       return t * TRAFFIC_MULT
   return 0
 
+def print_time():
+  print("%02d:%02d:%02d" % (TIME[0], TIME[1], TIME[2]))
+
 def clock():
-  global TIME, HOUR
-  while True:
-    TIME = now()
-    print_now()
-    HOUR = get_hour()
+#  global TIME, HOUR, SECONDS, DAY, DATE
+#  while True:
+#    TIME = get_time()
+#    HOUR = get_hour()
+#    DATE = get_date()
+#    DAY = TIME.weekday()
+#    print_now()
+#    SECONDS += 1
     sleep(TICKRATE)
 
 def create_customer(names):
@@ -271,7 +283,9 @@ def create_customer(names):
   name = names[randint(0, len(names)-1)]
   conn, cur = get_db()
   c = Customer(name, conn, cur)
+  lock.acquire()
   print(f"  Creating new customer {c.name} shopping for {c.num_items} items for {c.ticks} seconds")
+  lock.release()
   t = thread.Thread(target=c.shop, args=())
   t.start()
   threads.append(t)
@@ -281,11 +295,14 @@ def init_traffic(days, names):
   global TRAFFIC
   day = find_day(days, WEEK[DAY])
   traffic = get_hour_traffic(days)
-  num = traffic + randint(0 - TRAFFIC_OFFSET, 0 + TRAFFIC_OFFSET)
-  for i in range(num):
+  if traffic != 0:
+    traffic = traffic + randint(0 - TRAFFIC_OFFSET, 0 + TRAFFIC_OFFSET)
+  for i in range(traffic):
     create_customer(names)
 
 def create_employee(emp_id, start_time, hours):
+  global EMPLOYEES
+  EMPLOYEES += 1
   e = Employee(emp_id, start_time, hours)
   t = thread.Thread(target=e.work, args=())
   t.start()
@@ -305,18 +322,20 @@ def init_employees(days):
     emp_id = row[0]
     start_time = row[1]
     hours = row[2]
-    if (start_time + hours) % 24 > hour:
-      create_employee(emp_id, start_time, hours)
+    work_hour = start_time
+    for i in range(0, hours):
+      if work_hour == hour:
+        create_employee(emp_id, start_time, hours)
+        break
+      work_hour = (work_hour + 1) % 24
     cur.close()
     conn.close()
 
 def check_employees(hour):
   conn, cur = get_db()
-  cur.close()
-  conn.close()
   day_start = WEEK[DAY].lower() + '_start'
   day_hours = WEEK[DAY].lower() + '_hours'
-  cur.execute("SELECT id, %s, %s, FROM schedules WHERE %s=%d" % (day_start, day_hours, day_start, hour))
+  cur.execute("SELECT id, %s, %s FROM schedules WHERE %s=%d" % (day_start, day_hours, day_start, hour))
   records = cur.fetchall()
   for row in records:
     create_employee(row[0], row[1], row[2])
@@ -324,8 +343,16 @@ def check_employees(hour):
   conn.close()
 
 def start(days, names):
+  global TIME, HOUR, SECONDS, DAY, DATE
   secs_in_hour = 3600
   while True:
+    TIME = get_time()
+    HOUR = get_hour()
+    DATE = get_date()
+    DAY = TIME.weekday()
+    print_now()
+    SECONDS += 1
+
     # Randomly generate new customers
     traffic = get_hour_traffic(days)
     r = randint(0, secs_in_hour)
@@ -336,6 +363,7 @@ def start(days, names):
     h = int(TIME.strftime("%H"))
     m = int(TIME.strftime("%M"))
     s = int(TIME.strftime("%S"))
+
     if (m == 0 and s == 0) or (m == 0 and s == 1):
       check_employees(h)
 
@@ -346,7 +374,7 @@ days = get_days()
 read_traffic(days)
 names = read_names()
 
-TIME = now()
+TIME = get_time()
 HOUR = get_hour()
 DATE = get_date()
 
@@ -355,7 +383,6 @@ init_employees(days)
 
 # Initiate traffic based on time
 init_traffic(days, names)
-print("TRAFFIC: ", TRAFFIC)
 
 # Start clock
 t = thread.Thread(target=clock, args=())
